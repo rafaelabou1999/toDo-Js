d5d77c1249da2d37d174c5c67bdf6439dcfd894d
rafaelabou1999 <rafaelabourdette@gmail.com>
2024-04-07 12:13:22 -0300

    adding tabs

--- js/script.js
+++ js/script.js
@@ -32,6 +32,7 @@ function isEmpty(){
 
 function createLi(){
     const li = document.createElement('li');
+    
     return li;
 }
 
@@ -40,8 +41,12 @@ function createList(inputTxt){
    li.textContent = inputTxt;
    li.style.display = "flex";
    li.style.flexDirection = 'row';
-   ul.appendChild(li);
+   li.setAttribute("data-task-id", allTasks.length);
+   ul.appendChild(li)
    createCheck(li);
+   li.classList.add('active');
+   allTasks.push(li);
+
    return li;
 }
 
@@ -63,53 +68,80 @@ function createBtnDelete(li){
 }
 
 function deleteTask(li){
+ const taskId = parseInt(li.getAttribute('data-task-id'));
  li.remove();
-}
-btnTask.addEventListener("click", () => {
-    isEmpty();
-    
-    if(input.value.trim() !== ''){
-        const listItem = createList(input.value);
-        const deleteButton = createBtnDelete(listItem); 
-   
-    
-        deleteButton.addEventListener("click", () => {
-            deleteTask(listItem); 
-        });
-        input.value = '';
-    }
-    
-    createCheck(input.value);
-})
 
-let isChecked = false;
+ if(li.classList.contains("completed")){
+    completeTasks = completeTasks.filter(task => parseInt(task.getAttribute('data-task-id')) !== taskId);
+ } else{
+    activeTasks = activeTasks.filter(task => parseInt(task.getAttribute('data-task-id')) !== taskId);
+ }
+ allTasks = allTasks.filter(task => parseInt(task.getAttribute('data-task-id')) !== taskId);
+ updateDisplay(allTasks);
+}
+
+let completeTasks = [];
+let activeTasks = [];
 function createCheck(li){
     const box = document.createElement('div');
     box.innerHTML = `<i class="fa-regular fa-square"></i>`;
     box.style.marginRight = "3%";
     li.insertBefore(box, li.firstChild);
     box.style.color = 'rgb(18, 149, 201)';
-
+    li.classList.add('allTasks');
     box.addEventListener("click", () => {
         box.innerHTML = `<i class="fa-solid fa-square-check"></i>`;
         box.style.color = 'rgb(18, 149, 201)';
         li.style.textDecoration = 'line-through';
+      
 
         if (!isChecked) {
             box.innerHTML = `<i class="fa-solid fa-square-check"></i>`;
             li.style.textDecoration = 'line-through';
             li.style.color = "#808080";
+            li.classList.add('completed');
+            li.classList.remove('active')
+            completeTasks.push(li);
         } else {
             box.innerHTML = `<i class="fa-regular fa-square"></i>`;
             li.style.textDecoration = 'none';
             li.style.color = "#000";
+            li.classList.remove('completed')
+            
         }
 
 
         isChecked = !isChecked;
+
+     
     })
     return box;
 }
+
+let allTasks = [];
+let listItem = '';
+btnTask.addEventListener("click", () => {
+    isEmpty();
+  
+    if(input.value.trim() !== ''){
+        
+        listItem = createList(input.value);
+        const deleteButton = createBtnDelete(listItem); 
+        
+    
+        deleteButton.addEventListener("click", () => {
+            deleteTask(listItem); 
+        });
+        input.value = '';
+
+       return listItem;
+    }
+    createCheck(input.value);
+})
+
+let isChecked = false;
+
+
 
 input.addEventListener("keypress", (e) => {
   if(e.keyCode === 13){
@@ -127,7 +159,39 @@ input.addEventListener("keypress", (e) => {
         input.value = '';
     }
 
+    
     createCheck(input.value);
   }
 })
 
+const activeTab = document.querySelector(".activeTab");
+const allTab = document.querySelector(".allTab");
+const completedTab = document.querySelector(".completedTab");
+const isActiveTab = false;
+
+function updateDisplay(tasks){
+    ul.innerHTML = '';
+    tasks.forEach(task => {
+        ul.appendChild(task);
+    })
+}
+
+allTab.addEventListener("click", () => {
+    allTab.style.color = 'rgb(18, 149, 201)';
+    completedTab.style.color = '';
+    activeTab.style.color = '';
+    updateDisplay(allTasks);
+})
+completedTab.addEventListener("click", () => {
+    allTab.style.color = '';
+    completedTab.style.color = 'rgb(18, 149, 201)';
+    activeTab.style.color = '';
+    updateDisplay(completeTasks);
+})
+activeTab.addEventListener("click", () => {
+    allTab.style.color = '';
+    completedTab.style.color = '';
+    activeTab.style.color = 'rgb(18, 149, 201)';
+    const newArray = allTasks.filter(item => item.classList.contains('active'));
+    updateDisplay(newArray);
+})
